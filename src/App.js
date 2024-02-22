a63d4d7c7fdd124af24dc18dcdb4f3b69d624f02
chavaqui <chavaquivq@gmail.com>
2024-02-21 19:37:05 -0500

    added some different ways to center a div :/

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
 
+// const myPage = (
+//   <h1></h1>
+//   <h2></h2>
+//   <h1></h1>
+//   <h1></h1>
+// )
+const header1 = document.createElement('h1');
+header1.textContent = 'chemycal rage';
+header1.className = 'classTop'
+
+const text1 =  document.createElement('span');
+text1.textContent = 'I am a relateive text';
+text1.className = 'class span higher';
+
+export const list1 = document.createElement('button');
+list1.textContent = 'myButtonX';
+
+const myPage = document.getElementById('root');
+myPage.append(header1);
+myPage.append(text1);
+myPage.append(list1);
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+      </ul>
     </div>
   );
 }
 
-export default App;
+function MyComponentX() {
+  return (
+    <div>
+      <h1>HELLOW</h1>
+      <span>It is the coment lo g</span>
+      <div className='compose-center-react min-width-500'>
+        <h3 className='title-center-react'>Hi Chav_!</h3>
+        <p className='text-center-react'>It is the content text of my pharagrap and contains some interesting messages, like "Have fun wiriting code."</p>
+      </div>
+      <div className='compose-img-card'>
+        <h2 className='compose-img-title'>My responsive image</h2>
+        <img src={logo} alt='img logo' width='200px'></img>
+      </div>
+      <div className='footer-react'>
+        <span className='description-footer'>It is the footer</span>
+      </div>
+    </div>
+  );
+}
+
+const  ExportedToReactApp = {
+  App,MyComponentX
+}
+
+export default ExportedToReactApp;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import logo from './logo.svg'
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import ExportedToReactApp from './App';
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <img src={logo} alt="img" width="400px"></img>
+  <ExportedToReactApp.MyComponentX/>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
